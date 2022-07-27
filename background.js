a67661b5915ec81977f69b2a6a740ac61f0c7d3a
Calvin KW Chan <cp_itmd_32@labour.gov.hk>
2022-07-27 14:17:25 +0800

    use chrome storage to save the hotkey

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 let color = '#FF0000';
+let hotkey = 113;
 
 chrome.runtime.onInstalled.addListener(()=>{
-    chrome.storage.sync.set({color});
+    chrome.storage.sync.set({ hotkey : hotkey });
     console.log("yttc: new installed");
 });
